df76b71ee68825bd0bd4a4b8da4d757970a9727b
BMarin <bryanmarin2003@hotmail.com>
2024-12-18 16:41:07 -0500

    feat: Registro de usuarios arreglados

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -22,45 +22,64 @@ export class RegisterComponent {
   registerForm: FormGroup;
   selectedRole: string | null = null;
   isAdminSelected: boolean = false;
-
   showAdminDialog: boolean = false;
   adminPassword: string = '';
+  maxDate: string;
 
   constructor(
     private fb: FormBuilder,
     private registerService: RegisterService,
     private router: Router
   ) {
+    const today = new Date();
+    this.maxDate = today.toISOString().split('T')[0];
+
     this.registerForm = this.fb.group(
       {
-        username: [''],
+        username: ['', Validators.required],
         password: ['', Validators.required],
         confirmPassword: ['', Validators.required],
-        name: [''],
+        name: ['', Validators.required],
         paternalSurname: ['', Validators.required],
         maternalSurname: ['', Validators.required],
-        dni: ['', [Validators.required, Validators.pattern('\\d{8}')]],
+        dni: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]],
         email: [
           '',
           [
-            Validators.required, Validators.email,
+            Validators.required,
+            Validators.email,
             Validators.pattern(
               /^[a-zA-ZñÑ0-9._%+-]+@[a-zA-ZñÑ0-9.-]+\.[a-zA-Z]{2,4}$/
             ),
           ],
         ],
-        address: [''],
-        birthdate: ['', Validators.required],
-        phone: ['', Validators.required],
-        phoneBackup: ['', Validators.required],
+        address: ['', Validators.required],
+        birthdate: ['', [Validators.required, this.dateRangeValidator]],
+        phone: ['', [Validators.required, Validators.pattern('^[0-9]{9}$')]],
+        phoneBackup: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]{9}$')],
+        ],
         role: ['', Validators.required],
         isAdmin: [false],
-        paymentPerSession: [null],
-        paymentPerMonth: [null],
+        paymentSession: [null],
+        paymentMonth: [null],
         adminPassword: [''],
       },
       { validators: this.passwordsMatchValidator }
     );
+  }
+
+  dateRangeValidator(control: AbstractControl): ValidationErrors | null {
+    const date = new Date(control.value);
+    const minDate = new Date('1900-01-01');
+    const maxDate = new Date();
+
+    if (date < minDate || date > maxDate) {
+      return { outOfRange: true };
+    }
+
+    return null;
   }
 
   passwordsMatchValidator(form: AbstractControl): ValidationErrors | null {
@@ -72,62 +91,68 @@ export class RegisterComponent {
       : null;
   }
 
+  limitInputLength(event: KeyboardEvent, maxLength: number): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.value.length >= maxLength && event.key !== 'Backspace') {
+      event.preventDefault();
+    }
+  }
+
   selectRole(role: string): void {
     let backendRole: string;
 
     if (role === 'Terapeuta') {
       backendRole = 'THERAPIST';
+      this.registerForm
+        .get('paymentSession')
+        ?.setValidators([Validators.required, Validators.min(0)]);
+      this.registerForm.get('paymentMonth')?.clearValidators();
     } else if (role === 'Secretario/a') {
       backendRole = 'SECRETARY';
-    } else if (role === 'Admin') {
-      backendRole = 'ADMIN';
+      this.registerForm
+        .get('paymentMonth')
+        ?.setValidators([Validators.required, Validators.min(0)]);
+      this.registerForm.get('paymentSession')?.clearValidators();
     } else {
       backendRole = '';
+      this.registerForm.get('paymentSession')?.clearValidators();
     }
 
     this.selectedRole = role;
     this.registerForm.patchValue({ role: backendRole });
 
-    this.registerForm.get('paymentPerSession')?.clearValidators();
-    this.registerForm.get('paymentPerMonth')?.clearValidators();
-
-    if (role === 'Terapeuta') {
-      this.registerForm.get('paymentPerSession')?.setValidators([Validators.min(1)]);
-    } else if (role === 'Secretario/a') {
-      this.registerForm.get('paymentPerMonth')?.setValidators([Validators.min(1)]);
-    }
-    
-
-    this.registerForm.get('paymentPerSession')?.updateValueAndValidity();
-    this.registerForm.get('paymentPerMonth')?.updateValueAndValidity();
-
-    this.registerForm.get('adminPassword')?.clearValidators();
-    this.registerForm.get('adminPassword')?.updateValueAndValidity();
+    this.registerForm.get('paymentSession')?.updateValueAndValidity();
+    this.registerForm.get('paymentMonth')?.updateValueAndValidity();
   }
 
   promptAdminPassword(event: MouseEvent): void {
     if (event) {
       event.preventDefault();
     }
-  
+
     this.registerForm.patchValue({ adminPassword: '' });
-  
+
     this.registerForm
       .get('adminPassword')
       ?.setValidators([Validators.required]);
     this.registerForm.get('adminPassword')?.updateValueAndValidity();
-  
+
     this.showAdminDialog = true;
   }
 
   toggleAdminSelection(event: MouseEvent): void {
     const checkbox = event.target as HTMLInputElement;
-    
+
     if (checkbox.checked) {
       this.promptAdminPassword(event);
     } else {
       this.isAdminSelected = false;
       this.registerForm.patchValue({ isAdmin: false });
+
+      // Eliminar el validador de adminPassword
+      this.registerForm.get('adminPassword')?.clearValidators();
+      this.registerForm.get('adminPassword')?.updateValueAndValidity();
     }
   }
 
@@ -142,7 +167,7 @@ export class RegisterComponent {
       this.registerForm.get('adminPassword')?.clearValidators();
       this.registerForm.get('adminPassword')?.updateValueAndValidity();
     } else {
-      alert('Contraseña incorrecta');
+      this.registerForm.get('adminPassword')?.setErrors({ invalidPassword: true });
     }
   }
 
@@ -156,41 +181,34 @@ export class RegisterComponent {
   }
 
   onSubmit(): void {
-    
-    if (this.registerForm.valid) {
-      const confirmed = window.confirm(
-        '¿Estás seguro de que deseas guardar este registro?'
-      );
+    if (this.registerForm.valid && this.selectedRole) {
+      const formValue = this.registerForm.value;
 
-      if (confirmed) {
-        const formValue = this.registerForm.value;
-
-        if (this.selectedRole === 'Terapeuta') {
-          formValue.paymentPerSession = formValue.paymentPerSession 
-          ? (Number(formValue.paymentPerSession) * 1.0) 
+      if (this.selectedRole === 'Terapeuta') {
+        formValue.paymentSession = formValue.paymentSession
+          ? Number(formValue.paymentSession)
           : null;
-          formValue.paymentPerMonth = null;
-        } else if (this.selectedRole === 'Secretario/a') {
-          formValue.paymentPerMonth = formValue.paymentPerMonth 
-          ? (Number(formValue.paymentPerMonth) * 1.0) 
-          : 0.0;
-          formValue.paymentPerSession = null;
-        } else {
-          formValue.paymentPerSession = null;
-          formValue.paymentPerMonth = null;
-        }
-
-        this.registerService.registerUser(formValue).subscribe(
-          (response) => {
-            this.router.navigate(['/users']);
-          },
-          (error) => {
-            console.error('Error al registrar', error);
-          }
-        );
+        formValue.paymentMonth = null;
+      } else if (this.selectedRole === 'Secretario/a') {
+        formValue.paymentMonth = formValue.paymentMonth
+          ? Number(formValue.paymentMonth)
+          : null;
+        formValue.paymentSession = null;
+      } else {
+        formValue.paymentSession = null;
+        formValue.paymentMonth = null;
       }
+
+      this.registerService.registerUser(formValue).subscribe(
+        () => {
+          this.router.navigate(['/users']);
+        },
+        (error) => {
+          console.error('Error al registrar', error);
+        }
+      );
     } else {
-      console.error('Formulario inválido');
+      console.error('Formulario inválido o rol no seleccionado');
     }
   }
 
--- src/app/components/users/users.service.ts
+++ src/app/components/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
-import {environment} from "../../enviroment";
+import { environment } from '../../enviroment';
 
 @Injectable({
   providedIn: 'root',
@@ -13,39 +13,41 @@ export class UsersService {
 
   // Método para login y guardar el token
   login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { username, password }).pipe(
-      tap(response => {
-        if (this.isBrowser()) {
-          // Guardamos el token en localStorage solo si estamos en el navegador
-          localStorage.setItem('token', response.token);
-        }
-      })
-    );
+    return this.http
+      .post<any>(`${this.apiUrl}/login`, { username, password })
+      .pipe(
+        tap((response) => {
+          if (this.isBrowser()) {
+            // Guardamos el token en localStorage solo si estamos en el navegador
+            localStorage.setItem('token', response.token);
+          }
+        })
+      );
   }
 
   // Método para obtener usuarios desde el backend
   getUsers(): Observable<any[]> {
-    const token = this.getToken(); 
+    const token = this.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<any[]>(`${this.apiUrl}/all`, { headers });
   }
 
   // Método para obtener un usuario por su ID
   getUserDetails(userId: number): Observable<any> {
-    const token = this.getToken(); 
+    const token = this.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<any>(`${this.apiUrl}/select/${userId}`, { headers });
   }
 
   // Método para verificar si el usuario está logeado
   isLoggedIn(): boolean {
-    const token = this.getToken();  // Verifica si existe un token
-    return !!token;  // Devuelve true si el token existe, de lo contrario false
+    const token = this.getToken(); // Verifica si existe un token
+    return !!token; // Devuelve true si el token existe, de lo contrario false
   }
 
   // Método para cerrar sesión
   logout(): void {
-    localStorage.removeItem('token'); 
+    localStorage.removeItem('token');
   }
 
   private getToken(): string | null {
