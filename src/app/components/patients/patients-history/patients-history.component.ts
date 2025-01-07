d3a908d63b50ac356583f08a23ae09417621f95b
FluidityCore <Bertlas.u@gmail.com>
2025-01-06 19:36:40 -0500

    Limpieza de patient History

--- src/app/components/patients/patients-history/patients-history.component.ts
+++ src/app/components/patients/patients-history/patients-history.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EvaluationDocumentService} from "../evaluation-document.service";
 import { CommonModule } from "@angular/common";
 
 @Component({
@@ -13,10 +12,9 @@ import { CommonModule } from "@angular/common";
 export class PatientsHistoryComponent implements OnInit {
   patient: any;
   history: any[] = [];
-  documents: any[] = [];
   isLoading = true;
 
-  constructor(private evaluationDocumentService: EvaluationDocumentService, private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.loadPatientHistory();
@@ -24,61 +22,7 @@ export class PatientsHistoryComponent implements OnInit {
 
   loadPatientHistory(): void {
     const patientId = this.route.snapshot.params['id'];
-    this.evaluationDocumentService.getDocumentsByPatientId(patientId).subscribe(
-      (documents) => {
-        this.documents = documents;
-        this.isLoading = false;
-      },
-      (error) => {
-        console.error('Error fetching documents', error);
-        this.isLoading = false;
-      }
-    );
-  }
-
-  downloadDocument(documentId: number): void {
-    this.evaluationDocumentService.download(documentId).subscribe(
-      (blob) => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'document.pdf';
-        a.click();
-        window.URL.revokeObjectURL(url);
-      },
-      (error) => {
-        console.error('Error downloading document', error);
-      }
-    );
-  }
-
-  getDocumentById(documentId: number): void {
-    this.evaluationDocumentService.getById(documentId).subscribe(
-      (document) => {
-        console.log('Document:', document);
-        // Handle the document as needed
-      },
-      (error) => {
-        console.error('Error fetching document', error);
-      }
-    );
-  }
-
-  addDocument(): void {
-    const newDocument = {
-      name: 'Nuevo Documento',
-      description: 'Descripción del nuevo documento',
-      documentType: 'Tipo de Documento'
-    };
-    const file = new File([''], 'dummy.pdf'); // Archivo de ejemplo, reemplazar según sea necesario
-
-    this.evaluationDocumentService.create(newDocument, file).subscribe(
-      (document) => {
-        this.documents.push(document);
-      },
-      (error) => {
-        console.error('Error creando documento', error);
-      }
-    );
+    // Aquí puedes agregar la lógica para cargar el historial del paciente
+    this.isLoading = false;
   }
 }
