d592d8c10d555dc8c5c3a6b1c78a514b5b8239f6
Adrián Cisneros <nicolocisneros@gmail.com>
2025-01-06 20:55:43 -0500

    err: restore register patient

--- src/app/components/patients/register-patient.ts
+++ src/app/components/patients/register-patient.ts
@@ -1,18 +1,1 @@
-import { Tutor } from './tutor';
-
-export interface RegisterPatient {
-  name: string;
-  paternalSurname: string;
-  maternalSurname: string;
-  dni: string;
-  birthDate: string;
-  presumptiveDiagnosis: string;
-  age: number;
-  status: boolean;
-  idPlan: number;
-  tutors: Tutor[];
-  therapistId: number;
-  roomId: number;
-  startTime: string;
-  firstWeekDates: string[];
-}
+XDDDDDDD
