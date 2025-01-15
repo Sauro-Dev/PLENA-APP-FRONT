8f3d8c7fe42f7583022fe966152bf55d2bf7ec66
Adrian Cisneros <nicolocisneros@gmail.com>
2025-01-15 13:29:41 -0500

    fix: Importe de CommonModule en report.component

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users/users.service';
-import { NgForOf } from '@angular/common';
+import {CommonModule, NgForOf} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,8 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './reports.component.html',
   imports: [
     NgForOf,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   styleUrls: ['./reports.component.css']
 })
